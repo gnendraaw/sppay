8c0f3eb50561c5d0e537a3d77176b865e801c63a
gnendraaw <itsgnendraaw@gmail.com>
2023-02-27 06:26:40 +0800

    update db

--- sppay.sql
+++ sppay.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 23, 2023 at 09:34 AM
+-- Generation Time: Feb 26, 2023 at 10:26 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -20,6 +20,32 @@ SET time_zone = "+00:00";
 --
 -- Database: `sppay`
 --
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addPengguna` (`in_username` VARCHAR(32), `in_password` VARCHAR(32), `in_level` INT)   begin 
+insert into pengguna values(null, in_username, in_password, in_level);
+end$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addPetugas` (`in_username` VARCHAR(32), `in_nama` VARCHAR(32), `in_pengguna` INT)   begin 
+insert into petugas values(null, in_username, in_nama, in_pengguna);
+end$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addSiswa` (IN `in_nisn` VARCHAR(10), IN `in_nis` VARCHAR(5), IN `in_nama` VARCHAR(50), IN `in_telp` VARCHAR(13), IN `in_alamat` TEXT, IN `in_kelas` INT, IN `in_spp` INT, IN `in_pengguna` INT)   begin 
+insert into siswa values(null, in_nisn, in_nis, in_nama, in_telp, in_alamat, in_kelas, in_spp, in_pengguna);
+end$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updatePengguna` (`in_username` VARCHAR(32), `in_level` INT, `in_id_pengguna` INT)   begin 
+update pengguna set username=in_username, level=in_level where id_pengguna = in_id_pengguna;
+end$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updatePetugas` (`in_username` VARCHAR(32), `in_nama` VARCHAR(32), `in_id_petugas` INT)   begin 
+update petugas set username=in_username, nama_petugas=in_nama where id_petugas=in_id_petugas;
+end$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -40,7 +66,7 @@ CREATE TABLE `kelas` (
 INSERT INTO `kelas` (`id_kelas`, `nama_kelas`, `kompetensi_keahlian`) VALUES
 (2, 'RPL', 'Rekayasa Perangkat Lunak'),
 (3, 'MM', 'Multimedia'),
-(5, 'testst', 'estast');
+(9, 'TKJ', 'Teknik Komputer Jaringan');
 
 -- --------------------------------------------------------
 
@@ -79,59 +105,26 @@ CREATE TABLE `pembayaran` (
   `jumlah_bayar` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `pembayaran`
+-- Table structure for table `pengguna`
 --
 
-INSERT INTO `pembayaran` (`id_pembayaran`, `id_spp`, `id_siswa`, `id_petugas`, `tgl_bayar`, `bulan_bayar`, `tahun_bayar`, `jumlah_bayar`) VALUES
-(2, 11, 4, 1, '2023-02-22', 'juni', '2023', 200000),
-(4, 11, 4, 1, '2023-02-22', 'juli', '2023', 200000),
-(5, 11, 4, 1, '2023-02-22', 'agustus', '2023', 200000),
-(6, 11, 4, 1, '2023-02-22', 'september', '2023', 200000),
-(7, 11, 4, 1, '2023-02-22', 'oktober', '2023', 200000),
-(8, 11, 4, 1, '2023-02-22', 'november', '2023', 200000),
-(9, 11, 4, 1, '2023-02-22', 'desember', '2023', 200000),
-(10, 11, 4, 1, '2023-02-22', 'januari', '2023', 200000),
-(11, 11, 4, 1, '2023-02-22', 'februari', '2023', 200000),
-(12, 11, 4, 1, '2023-02-22', 'maret', '2023', 200000),
-(13, 11, 4, 1, '2023-02-22', 'april', '2023', 200000),
-(14, 11, 4, 1, '2023-02-22', 'mei', '2023', 200000),
-(39, 11, 7, 1, '2023-02-22', 'juli', '2023', 200000),
-(40, 11, 7, 1, '2023-02-22', 'agustus', '2023', 200000),
-(41, 11, 7, 1, '2023-02-22', 'september', '2023', 200000),
-(42, 11, 7, 1, '2023-02-22', 'oktober', '2023', 200000),
-(43, 11, 7, 1, '2023-02-22', 'november', '2023', 200000),
-(44, 11, 7, 1, '2023-02-22', 'desember', '2023', 200000),
-(45, 11, 7, 1, '2023-02-22', 'januari', '2023', 200000),
-(46, 11, 7, 1, '2023-02-22', 'februari', '2023', 200000),
-(47, 11, 7, 1, '2023-02-22', 'maret', '2023', 200000),
-(48, 11, 7, 1, '2023-02-22', 'april', '2023', 200000),
-(49, 11, 7, 1, '2023-02-22', 'juni', '2023', 200000),
-(50, 11, 7, 1, '2023-02-22', 'mei', '2023', 200000),
-(51, 11, 8, 1, '2023-02-22', 'juli', '2023', 200000),
-(52, 11, 8, 1, '2023-02-22', 'agustus', '2023', 200000),
-(53, 11, 8, 1, '2023-02-22', 'september', '2023', 200000),
-(54, 11, 8, 1, '2023-02-22', 'oktober', '2023', 200000),
-(55, 11, 8, 1, '2023-02-22', 'november', '2023', 200000),
-(56, 11, 8, 1, '2023-02-22', 'desember', '2023', 200000),
-(57, 11, 9, 1, '2023-02-23', 'juli', '2023', 200000),
-(58, 11, 9, 1, '2023-02-23', 'agustus', '2023', 200000),
-(59, 11, 9, 1, '2023-02-23', 'september', '2023', 200000),
-(60, 11, 9, 1, '2023-02-23', 'oktober', '2023', 200000),
-(61, 11, 9, 1, '2023-02-23', 'november', '2023', 200000),
-(62, 11, 9, 1, '2023-02-23', 'januari', '2023', 200000),
-(63, 11, 9, 1, '2023-02-23', 'desember', '2023', 200000),
-(64, 11, 9, 1, '2023-02-23', 'februari', '2023', 200000),
-(65, 11, 9, 1, '2023-02-23', 'maret', '2023', 200000),
-(66, 11, 9, 1, '2023-02-23', 'april', '2023', 200000),
-(67, 11, 9, 1, '2023-02-23', 'mei', '2023', 200000),
-(68, 11, 9, 1, '2023-02-23', 'juni', '2023', 200000),
-(69, 11, 8, 1, '2023-02-23', 'januari', '2023', 200000),
-(70, 11, 8, 1, '2023-02-23', 'februari', '2023', 200000),
-(71, 11, 8, 1, '2023-02-23', 'maret', '2023', 200000),
-(72, 11, 8, 1, '2023-02-23', 'april', '2023', 200000),
-(73, 11, 8, 1, '2023-02-23', 'mei', '2023', 200000),
-(74, 11, 8, 1, '2023-02-23', 'juni', '2023', 200000);
+CREATE TABLE `pengguna` (
+  `id_pengguna` int NOT NULL,
+  `username` varchar(32) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `level` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `pengguna`
+--
+
+INSERT INTO `pengguna` (`id_pengguna`, `username`, `password`, `level`) VALUES
+(1, 'admin', 'e64b78fc3bc91bcbc7dc232ba8ec59e0', 1),
+(2, 'petugas', '6b3402d7cea9dfb83a5278657380082e', 2);
 
 -- --------------------------------------------------------
 
@@ -142,17 +135,35 @@ INSERT INTO `pembayaran` (`id_pembayaran`, `id_spp`, `id_siswa`, `id_petugas`, `
 CREATE TABLE `petugas` (
   `id_petugas` int NOT NULL,
   `username` varchar(32) DEFAULT NULL,
-  `password` varchar(32) DEFAULT NULL,
   `nama_petugas` varchar(32) DEFAULT NULL,
-  `id_level` int DEFAULT NULL
+  `id_pengguna` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `petugas`
 --
 
-INSERT INTO `petugas` (`id_petugas`, `username`, `password`, `nama_petugas`, `id_level`) VALUES
-(1, 'admin', 'admin123', 'admin', 1);
+INSERT INTO `petugas` (`id_petugas`, `username`, `nama_petugas`, `id_pengguna`) VALUES
+(1, 'admin', 'admin', 1),
+(2, 'petugas', 'petugas', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `selectpetugas`
+-- (See below for the actual view)
+--
+CREATE TABLE `selectpetugas` (
+`id_petugas` int
+,`username_petugas` varchar(32)
+,`nama_petugas` varchar(32)
+,`id_pengguna` int
+,`username` varchar(32)
+,`password` varchar(32)
+,`level` int
+,`id_level` int
+,`keterangan` varchar(12)
+);
 
 -- --------------------------------------------------------
 
@@ -165,22 +176,12 @@ CREATE TABLE `siswa` (
   `nisn` varchar(10) NOT NULL,
   `nis` varchar(5) DEFAULT NULL,
   `nama_siswa` varchar(50) NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
   `no_telp` varchar(13) DEFAULT NULL,
   `alamat` text,
   `id_kelas` int DEFAULT NULL,
-  `id_spp` int DEFAULT NULL
+  `id_spp` int DEFAULT NULL,
+  `id_pengguna` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `siswa`
---
-
-INSERT INTO `siswa` (`id_siswa`, `nisn`, `nis`, `nama_siswa`, `password`, `no_telp`, `alamat`, `id_kelas`, `id_spp`) VALUES
-(4, '288828', '9999', 'budi sudirman', 'f1beaf277a0ba2efdec5ed4ce8f5435b', '08123456789', 'alamat budi sudirman', 2, 11),
-(7, '1342352353', '23525', 'rudy santoso', 'f1beaf277a0ba2efdec5ed4ce8f5435b', '2343423523525', 'alamat rudy santoso', 2, 11),
-(8, '9991288488', '12412', 'budi irawan', 'f1beaf277a0ba2efdec5ed4ce8f5435b', '1355135312515', 'alamat budi irawan', 2, 11),
-(9, '2222222222', '22222', 'annak baru', 'f1beaf277a0ba2efdec5ed4ce8f5435b', '1328888888888', 'alamat nak baru', 2, 11);
 
 -- --------------------------------------------------------
 
@@ -194,12 +195,14 @@ CREATE TABLE `spp` (
   `nominal` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `spp`
---
+-- --------------------------------------------------------
 
-INSERT INTO `spp` (`id_spp`, `tahun`, `nominal`) VALUES
-(11, 2023, 200000);
+--
+-- Structure for view `selectpetugas`
+--
+DROP TABLE IF EXISTS `selectpetugas`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `selectpetugas`  AS SELECT `p`.`id_petugas` AS `id_petugas`, `p`.`username` AS `username_petugas`, `p`.`nama_petugas` AS `nama_petugas`, `pen`.`id_pengguna` AS `id_pengguna`, `pen`.`username` AS `username`, `pen`.`password` AS `password`, `pen`.`level` AS `level`, `l`.`id_level` AS `id_level`, `l`.`keterangan` AS `keterangan` FROM ((`petugas` `p` left join `pengguna` `pen` on((`pen`.`id_pengguna` = `p`.`id_pengguna`))) left join `level` `l` on((`pen`.`level` = `l`.`id_level`)))  ;
 
 --
 -- Indexes for dumped tables
@@ -227,11 +230,18 @@ ALTER TABLE `pembayaran`
   ADD KEY `id_spp` (`id_spp`);
 
 --
+-- Indexes for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`id_pengguna`),
+  ADD KEY `level` (`level`);
+
+--
 -- Indexes for table `petugas`
 --
 ALTER TABLE `petugas`
   ADD PRIMARY KEY (`id_petugas`),
-  ADD KEY `id_level` (`id_level`);
+  ADD KEY `id_pengguna` (`id_pengguna`);
 
 --
 -- Indexes for table `siswa`
@@ -239,7 +249,8 @@ ALTER TABLE `petugas`
 ALTER TABLE `siswa`
   ADD PRIMARY KEY (`id_siswa`),
   ADD KEY `fk_spp` (`id_spp`),
-  ADD KEY `id_kelas` (`id_kelas`);
+  ADD KEY `id_kelas` (`id_kelas`),
+  ADD KEY `id_pengguna` (`id_pengguna`);
 
 --
 -- Indexes for table `spp`
@@ -255,7 +266,7 @@ ALTER TABLE `spp`
 -- AUTO_INCREMENT for table `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id_kelas` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_kelas` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `level`
@@ -267,7 +278,13 @@ ALTER TABLE `level`
 -- AUTO_INCREMENT for table `pembayaran`
 --
 ALTER TABLE `pembayaran`
-  MODIFY `id_pembayaran` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+  MODIFY `id_pembayaran` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  MODIFY `id_pengguna` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `petugas`
@@ -279,13 +296,13 @@ ALTER TABLE `petugas`
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id_siswa` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_siswa` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `spp`
 --
 ALTER TABLE `spp`
-  MODIFY `id_spp` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_spp` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -300,17 +317,24 @@ ALTER TABLE `pembayaran`
   ADD CONSTRAINT `pembayaran_ibfk_4` FOREIGN KEY (`id_spp`) REFERENCES `spp` (`id_spp`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD CONSTRAINT `pengguna_ibfk_1` FOREIGN KEY (`level`) REFERENCES `level` (`id_level`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `petugas`
 --
 ALTER TABLE `petugas`
-  ADD CONSTRAINT `petugas_ibfk_1` FOREIGN KEY (`id_level`) REFERENCES `level` (`id_level`) ON DELETE CASCADE;
+  ADD CONSTRAINT `petugas_ibfk_2` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `siswa`
 --
 ALTER TABLE `siswa`
   ADD CONSTRAINT `fk_spp` FOREIGN KEY (`id_spp`) REFERENCES `spp` (`id_spp`) ON DELETE CASCADE,
-  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE;
+  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE,
+  ADD CONSTRAINT `siswa_ibfk_2` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
