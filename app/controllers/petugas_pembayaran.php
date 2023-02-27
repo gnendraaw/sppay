ceaaf0812228bb39f7211bf535dfb41883bc5973
gnendraaw <itsgnendraaw@gmail.com>
2023-02-27 17:44:23 +0800

    update pembayaran detail page (petuga)

--- app/controllers/petugas_pembayaran.php
+++ app/controllers/petugas_pembayaran.php
@@ -26,7 +26,12 @@ class Petugas_pembayaran extends Controller {
         $siswa = $this->model('siswa_model')->getSiswaById($id);
         $pembayaran = $this->model('pembayaran_model')->getPembayaranBySiswaId($siswa['id_siswa']);
         $bulan = [
-            'juli', 'agustus', 'september', 'oktober', 'november', 'desember', 'januari', 'februari', 'maret', 'april', 'mei', 'juni',
+            [
+                'juli', 'agustus', 'september', 'oktober' , 'november', 'desember',
+            ],
+            [
+                'januari', 'februari', 'maret', 'april', 'mei', 'juni',
+            ],
         ];
         $bulan_terbayar = [];
         foreach($pembayaran as $p) {
--- app/views/petugas/pembayaran/detail.php
+++ app/views/petugas/pembayaran/detail.php
@@ -32,32 +32,37 @@
             <div class="card mb-3">
                 <div class="card-body py-3">
                     <form action="<?=BASE_URL?>/petugas-pembayaran/store" method="post" id="adminBayarSppForm">
-                    <div class="row">
                         <input type="hidden" name="nominal" value="<?=$data['siswa']['nominal']?>">
                         <input type="hidden" name="id_spp" value="<?=$data['siswa']['id_spp']?>">
                         <input type="hidden" name="id_siswa" value="<?=$data['siswa']['id_siswa']?>">
 
-                        <?php foreach($data['bulan'] as $bulan): ?>
-                            <div class="col-sm-12 col-md-6 col-xl-4 mb-3 spp-card">
-                                <input type="hidden" name="bulan[<?=$bulan?>]" value="false" data-bulan="<?=$bulan?>" id="inputBulan">
+                        <?php foreach($data['bulan'] as $key => $bulan): ?>
+                            <h6 class="text-grey-800 font-weight-bold">Semester <?=++$key?></h6>
 
-                                <?php if(in_array($bulan, $data['bulan_terbayar'])): ?>
-                                    <card class="card p-3 bg-success text-white">
-                                        <h6 class="text-uppercase"><?=$bulan?></h6>
-                                        <h4 class="text-grey-800 font-weight-bold">Rp <?=$data['siswa']['nominal']?></h4>
-                                    </card>
+                            <hr>
 
-                                <?php else: ?>
-                                    <button type="button" class="card p-3 btn-block">
-                                        <h6 class="text-uppercase"><?=$bulan?></h6>
-                                        <h4 class="text-grey-800 font-weight-bold">Rp <?=$data['siswa']['nominal']?></h4>
-                                    </button>
+                            <div class="row">
+                                <?php foreach($bulan as $bln): ?>
+                                        <div class="col-sm-12 col-md-6 col-xl-4 mb-3 spp-card">
+                                            <input type="hidden" name="bulan[<?=$bln?>]" value="false" data-bulan="<?=$bln?>" id="inputBulan">
 
-                                <?php endif ?>
+                                            <?php if(in_array($bln, $data['bulan_terbayar'])): ?>
+                                                <card class="card p-3 bg-success text-white">
+                                                    <h6 class="text-uppercase"><?=$bln?></h6>
+                                                    <h4 class="text-grey-800 font-weight-bold">Rp <?=$data['siswa']['nominal']?></h4>
+                                                </card>
+
+                                            <?php else: ?>
+                                                <button type="button" class="card p-3 btn-block">
+                                                    <h6 class="text-uppercase"><?=$bln?></h6>
+                                                    <h4 class="text-grey-800 font-weight-bold">Rp <?=$data['siswa']['nominal']?></h4>
+                                                </button>
+
+                                            <?php endif ?>
+                                        </div>
+                                <?php endforeach ?>
                             </div>
                         <?php endforeach ?>
-
-                    </div>
                 </div>
             </div>
             <button type="button" class="btn btn-success btn-block" id="adminBayarSppBtn" data-toggle="modal" data-target="#adminBayarSppModal">Bayar</button>
